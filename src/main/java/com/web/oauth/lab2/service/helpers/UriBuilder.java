1c013064cf86ea93d7f42a61ac36d0495b68bb54
Nikita <baby0benz@gmail.com>
2020-12-17 01:19:36 +0300

    add UriBuilder class for URI assembling

---
+++ src/main/java/com/web/oauth/lab2/service/helpers/UriBuilder.java
@@ -0,0 +1,28 @@
+package com.web.oauth.lab2.service.helpers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.Map;
+
+@Component
+public class UriBuilder {
+    @Value("${security.oauth2.authorization.base-uri}")
+    private String authorizationUri;
+    @Value("#{${security.oauth2.authorization.query-params}}")
+    private Map<String, String> authorizationQueryParams;
+
+    private static final String BASE_URI = "oauth.vk.com";
+
+    public URI buildAuthorizationUri() {
+        UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder.newInstance()
+                .scheme("https")
+                .host(BASE_URI)
+                .path(authorizationUri);
+        authorizationQueryParams.forEach(uriComponentsBuilder::queryParam);
+
+        return uriComponentsBuilder.build().toUri();
+    }
+}
