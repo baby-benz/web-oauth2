c9fd2d1f5207b552be0db5bc8557f491266ed014
Nikita <baby0benz@gmail.com>
2020-12-17 01:25:38 +0300

    add WebConfig class with GsonHttpMessageConverter for correct Gson serializing

---
+++ src/main/java/com/web/oauth/lab2/configuration/WebConfig.java
@@ -0,0 +1,18 @@
+package com.web.oauth.lab2.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.GsonHttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@EnableWebMvc
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(new GsonHttpMessageConverter());
+    }
+}
